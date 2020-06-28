52481fce7c77c30b0a6089be798b79eca9a0945c
llanderman <chemdeath69@googlemail.com>
2020-06-27 22:03:05 -0700

    Initial commit
    Initial Commit

---
+++ utorrent-prometheus-exporter/Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("uTorrent Prometheus Exporter")]
+[assembly: AssemblyDescription("Exporter for Prometheus Metrics from uTorrent using WebAPI")]
+#if DEBUG
+[assembly: AssemblyConfiguration("Debug")]
+#else
+[assembly: AssemblyConfiguration("Release")]
+#endif
+[assembly: AssemblyCompany("Nightrose Gaming")]
+[assembly: AssemblyProduct("uTorrent Prometheus Exporter")]
+[assembly: AssemblyCopyright("Copyright © Nightrose Gaming 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a61afc1c-e535-4beb-b0ec-78fabdf0b37f")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
